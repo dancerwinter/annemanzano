2327a56fc872619b5aa7fff74db4521be3c44fcb
dancerwinter <jessica.manzano@obf.ateneo.edu>
2021-11-02 15:41:34 +0800

    Finished react tutorial and installed bulma dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
+import TodoList from "./TodoList";
+import React, { useState, useRef, useEffect } from 'react';
+import {v4 as uuidv4} from 'uuid';
+import 'bulma/css/bulma.min.css';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  // Storing
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+    // Saving
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
   return (
-    null
-  );
+    <>
+        <div classname="main">
+            <div className="container">
+                <div class="navbar"></div>
+              <TodoList todos={todos} toggleTodo={toggleTodo} />
+              <input ref={todoNameRef} type='text' />
+              <button onClick={handleAddTodo}>Add</button>
+              <button onClick={handleClearTodos}>Clear</button>
+              <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+            </div>
+        </div>
+    </>
+  )
 }
 
 export default App;
